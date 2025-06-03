921112fc456e416a0dcc3b1f0d8ba547c497122a
astro-phel <katgranads@gmail.com>
2025-06-03 01:41:53 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,23 @@
 import java.util.ArrayList;
-public class StepTracker
-{
+public class StepTracker{
  /* to be implemented here */
+ private int minSteps, totalSteps;
+ private int totalDays, actDays;
+ public StepTracker(int goal){
+  minSteps = goal;
+  totalSteps = 0;
+  totalDays = 0;
+  actDays = 0;
+ }
+ public void addDailySteps(int steps){
+  totalSteps+=steps;
+  if(steps > minSteps)
+   activeDays++;
+ }
+ public int activeDays(){
+  return activeDays;
+ }
+ public double averageSteps(){
+  return totalSteps/activeDays;
+ }
 } 
